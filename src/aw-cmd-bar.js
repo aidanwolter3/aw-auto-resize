1491ccc6fd33c997e578252160cb5bc300c2b1a2
Aidan Wolter <aidanwolter@letu.edu>
2014-07-17 22:52:57 -0500

    converted tabs to spaces in aw-cmd-bar

--- src/aw-cmd-bar.js
+++ src/aw-cmd-bar.js
@@ -26,7 +26,7 @@ angular.module('awCmdBar', ['awFocusIf'])
       'commandTable': '=commands',
       'defaultPlaceholder': '=',
       'focusOn': '=?',
-			'inputContent': '=?'
+      'inputContent': '=?'
     },
 
     //create the html template
@@ -42,11 +42,11 @@ angular.module('awCmdBar', ['awFocusIf'])
       //user submitted a command
       scope.submitCommand = function() {
 
-				//verify that commandTable was set
-				if(!scope.commandTable) {
-					console.log('Commands are not set for aw-cmd-bar!');
-					return;
-				}
+        //verify that commandTable was set
+        if(!scope.commandTable) {
+          console.log('Commands are not set for aw-cmd-bar!');
+          return;
+        }
 
         //get the first part of the command
         var comm = scope.inputContent;
@@ -56,18 +56,18 @@ angular.module('awCmdBar', ['awFocusIf'])
         //check commandTable for exact matches
         var command = scope.commandTable[first];
 
-				//if no exact matches are found
-				if(!command) {
-					
-					//check if matches anything in commandTable as a regex
-					for(key in scope.commandTable) {
-						var patt = new RegExp(key);
-						if(patt.test(first)) {
-							command = scope.commandTable[key];
-							break;
-						}
-					}
-				}
+        //if no exact matches are found
+        if(!command) {
+          
+          //check if matches anything in commandTable as a regex
+          for(key in scope.commandTable) {
+            var patt = new RegExp(key);
+            if(patt.test(first)) {
+              command = scope.commandTable[key];
+              break;
+            }
+          }
+        }
 
         if(command) {
           command(comm);
@@ -79,7 +79,7 @@ angular.module('awCmdBar', ['awFocusIf'])
 
         //clear the input contents
         scope.inputContent = '';
-				scope.focusOn = false;
+        scope.focusOn = false;
       }
 
       //focus and blur on command bar events
@@ -87,20 +87,20 @@ angular.module('awCmdBar', ['awFocusIf'])
         scope.focusOn = true;
       }
       scope.didBlurCommandBar = function() {
-				scope.focusOn = false;
-				scope.inputContent = '';
+        scope.focusOn = false;
+        scope.inputContent = '';
       }
 
-			//watch the focus on trigger to update the command bar status
-			scope.$watch('focusOn', function() {
-				if(scope.focusOn) {
-					scope.focusOn = true;
-					scope.commandBarPlaceholder = '';
-				} else {
-					scope.focusOn = false;
-					scope.commandBarPlaceholder = scope.defaultPlaceholder;
-				}
-			});
+      //watch the focus on trigger to update the command bar status
+      scope.$watch('focusOn', function() {
+        if(scope.focusOn) {
+          scope.focusOn = true;
+          scope.commandBarPlaceholder = '';
+        } else {
+          scope.focusOn = false;
+          scope.commandBarPlaceholder = scope.defaultPlaceholder;
+        }
+      });
 
       //watch when to focus or cancel focus on the command bar
       scope.$watch('focusOn', function() {
